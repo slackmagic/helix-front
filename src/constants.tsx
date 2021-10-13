be23410c9935bb3e0b3c6ddc48ac75e1d0de2e23
SlackMagiC <laurent.pietrzyk@gmail.com>
2021-10-13 18:44:06 +0200

    🔧 Change homepage subheader

--- src/constants.tsx
+++ src/constants.tsx
@@ -1,5 +1,5 @@
 export const APP_TITLE: string = "HELIX";
-export const APP_SUBTITLE: string = "HOME SWEET HOME";
+export const APP_SUBTITLE: string = "SOMETHING NEW IS COMING !";
 //export const LOGIN_HOST: string = "https://helix.ovh";
 export const LOGIN_HOST: string = "";
 export const LOGIN_URL: string = "/l0gin";
